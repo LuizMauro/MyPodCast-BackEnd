f4788c840ea46b6fa6dd2e1a06ca85501ab65b0f
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-02-19 11:46:12 -0300

    procedures adicionadas

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const Categoria = require('./controllers/CategoriaController');
 const PodCast = require('./controllers/PodCastController');
 const PodcastCategoria = require('./controllers/PodcastCategoriaController');
 const Endereco = require('./controllers/EnderecoController');
-const CriarPodcastProcedure = require('./controllers/CriarPodcastProcedureController');
+const PodcastProcedure = require('./controllers/PodcastProcedureController');
 //final chamando os controllers
 
 const { date } = require('./utils/Date');
@@ -33,8 +33,8 @@ routes.post('/podcastctg', PodcastCategoria.store);
 routes.get('/endereco', Endereco.index);
 routes.post('/endereco', Endereco.store);
 
-routes.post('/criarpodcast', CriarPodcastProcedure.store);
-routes.put('/podcast/:id', CriarPodcastProcedure.update);
+routes.post('/criarpodcast', PodcastProcedure.store);
+routes.put('/podcast/:id', PodcastProcedure.update);
 
 routes.get('/getdate', (req, resp) => {
 	return resp.json({ data: date(Date.now()).format });
