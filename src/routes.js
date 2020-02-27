5ef9fe283becce93efdc06d2a0543e442c4a14c4
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-02-27 16:50:31 -0300

    informação de rotas privadas no Routes e database.js no gitignore

--- src/routes.js
+++ src/routes.js
@@ -31,54 +31,48 @@ const routes = express.Router();
 routes.get('/users', UserController.index);
 routes.get('/users/:usu_id', UserController.read);
 routes.post('/users', UserStoreValidate ,UserController.store);
-routes.put('/users/:usu_id/:usu_status', UserController.updateUserStatus);// precisa estar logado!?
-routes.put('/users/tipo/:usu_id/:tus_id', UserController.updateUsuarioTipo);// precisa estar logado!?
-routes.put('/edituser/:usu_id/', UserController.updateUserPerfil);// precisa estar logado!?
-routes.put('/usersenha/', authMiddleware ,UserController.updateUserSenha);// precisa estar logado!?
+routes.put('/users/:usu_id/:usu_status', UserController.updateUserStatus);// MODERADOR/ADM
+routes.put('/users/tipo/:usu_id/:tus_id', UserController.updateUsuarioTipo);// MODERADOR/ADM
+routes.put('/edituser/:usu_id/', UserController.updateUserPerfil);// QUALQUER USUARIO (EDITA O PROPRIO PERFIL)
+routes.put('/usersenha/:usu_id', authMiddleware ,UserController.updateUserSenha);// QUALQUER USUARIO (EDITA O PROPRIO PERFIL)
 
 // ou utilizar routes.use(authMiddleware);
 
 
 routes.post('/sessions', SessionController.store);
 
+/* APENAS TESTE INICIAL
 routes.get('/tipouser', TipoUser.index);// // precisa estar logado!?
 routes.post('/tipouser', TipoUser.store);// // precisa estar logado!?
+*/
 
 routes.get('/categoria', Categoria.index); 
-routes.post('/categoria', Categoria.store); // precisa estar logado!?
-routes.put('/categoria/:ctg_id', Categoria.updateCtgDescricao); // precisa estar logado!?
-routes.put('/categoria/:ctg_id/:ctg_status', Categoria.updateCtgStatus); // precisa estar logado!?
+routes.post('/categoria', Categoria.store); // MODERADOR E ADM
+routes.put('/categoria/:ctg_id', Categoria.updateCtgDescricao); // MODERADOR E ADM
+routes.put('/categoria/:ctg_id/:ctg_status', Categoria.updateCtgStatus); // MODERADOR E ADM
 
 routes.get('/podcasts', PodCast.index);
-routes.post('/podcast', upload.single('file'), PodcastProcedure.store); // precisa estar logado!?
-routes.put('/podcast/:pod_id', PodcastProcedure.update); // precisa estar logado!?
-routes.put(
-	'/podcastimg/:pod_id',
-	upload.single('file'),
-	PodCast.updatePodcastImg
-); // precisa estar logado!?
+routes.post('/podcast', upload.single('file'), PodcastProcedure.store); // PODCASTER E ADM
+routes.put('/podcast/:pod_id', PodcastProcedure.update); // PODCASTER, MODERADOR E ADM
+routes.put('/podcastimg/:pod_id',upload.single('file'),PodCast.updatePodcastImg); // PODCASTER, MODERADOR E ADM
+routes.put('/podcast/:pod_id/:pod_status', PodCast.updatePodcastStatus); // MODERADOR E ADM
 
+routes.get('/podcasts/solicitacao', SolicitacaoCadastro.index); // MODERADOR E ADM
+routes.put('/podcasts/solicitacao/:pod_id/:pod_permissao',SolicitacaoCadastro.update); // MODERADOR E ADM
 
-routes.put('/podcast/:pod_id/:pod_status', PodCast.updatePodcastStatus); // precisa estar logado!?
+//PESQUISA DE PODCAST
+routes.get('/podcastctg/:pod_id', PodcastCategoria.indexCtgByPodcastID); // TODOS
+routes.get('/pesquisar/:ctg_id', PodcastCategoria.indexPodcastByCtgID); //TODOS
+routes.get('/pesquisar/nome/:ctg_id', PodcastCategoria.indexPodcastByCtgNome); //TODOS
+routes.post('/podcastctg', PodcastCategoria.store); //TODOS
 
-routes.get('/podcasts/solicitacao', SolicitacaoCadastro.index);
-routes.put(
-	'/podcasts/solicitacao/:pod_id/:pod_permissao',
-	SolicitacaoCadastro.update
-);
+routes.get('/endereco', Endereco.index); //TODOS
+routes.post('/endereco', Endereco.store); //TODOS
 
-routes.get('/podcastctg/:pod_id', PodcastCategoria.indexCtgByPodcastID);
-routes.get('/pesquisar/:ctg_id', PodcastCategoria.indexPodcastByCtgID);
-routes.get('/pesquisar/nome/:ctg_id', PodcastCategoria.indexPodcastByCtgNome);
-routes.post('/podcastctg', PodcastCategoria.store);
-
-routes.get('/endereco', Endereco.index);
-routes.post('/endereco', Endereco.store);
-
-routes.get('/tag', Tag.index);
-routes.post('/tag', Tag.store);
-routes.put('/tag/:tag_id', Tag.updateTag);
-routes.put('/tag/:tag_id/:tag_status', Tag.updateTagStatus);
+routes.get('/tag', Tag.index); //TODOS
+routes.post('/tag', Tag.store); //TODOS
+routes.put('/tag/:tag_id', Tag.updateTag); //ADM
+routes.put('/tag/:tag_id/:tag_status', Tag.updateTagStatus); // ADM
 
 
 routes.get('/getdate', (req, resp) => {
