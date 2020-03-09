f879aabf8258962a2e3bd64bcd142f9594dc4d34
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-03-09 12:41:25 -0300

    fix select feedback perfil

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -41,7 +41,7 @@ class Feedback extends Model {
 	static async updateFavorito(podid, fbkstatus, usuid) {
 		try {
 			const [result] = await this.sequelize.query(
-				'update fbk_feedback set fbk_status = :fbk_status where pod_id = :pod_id and c.tfb_id = 1 and usu_id = :usu_id',
+				'update fbk_feedback set fbk_status = :fbk_status where pod_id = :pod_id and tfb_id = 1 and usu_id = :usu_id',
 				{
 					replacements: { pod_id: podid, fbk_status: fbkstatus, usu_id: usuid },
 					type: QueryTypes.UPDATE,
@@ -85,7 +85,7 @@ class Feedback extends Model {
 	//Visualiza todos os Favoritos feitos para um podcast
 	static async findFavorito(podid, usuid) {
 		const [results] = await this.sequelize.query(
-			'select a.usu_nome, b.pod_id, b.pod_nome, c.tfb_descricao, d.fbk_id, d.fbk_status from usu_usuario a join fbk_feedback d on a.usu_id  = d.usu_id join pod_podcast b on b.pod_id = d.pod_id join tfb_tipo_feedback c on c.tfb_id = d.tfb_id where b.pod_id = :pod_id and a.usu_id = :usu_id and d.fbk_status = 1',
+			'select a.usu_nome, b.pod_id, b.pod_nome, c.tfb_descricao, d.fbk_id, d.fbk_status from usu_usuario a join fbk_feedback d on a.usu_id  = d.usu_id join pod_podcast b on b.pod_id = d.pod_id join tfb_tipo_feedback c on c.tfb_id = d.tfb_id where b.pod_id = :pod_id and a.usu_id = :usu_id and c.tfb_id = 1 and d.fbk_status = 1;',
 			{
 				replacements: { pod_id: podid, usu_id: usuid },
 				type: QueryTypes.SELECT,
@@ -110,10 +110,10 @@ class Feedback extends Model {
 		return results;
 	}
 
-	//Visualiza todos os feedbacks feitos por um usuario
+	//Visualiza todos os Feedbacks de um usuário
 	static async findFeedbackUser(usuid) {
 		const results = await this.sequelize.query(
-			' select a.usu_nome, b.pod_nome, c.tfb_id, c.tfb_descricao, d.fbk_status from usu_usuario a join fbk_feedback d on a.usu_id  = d.usu_id join pod_podcast b on b.pod_id = d.pod_id join tfb_tipo_feedback c on c.tfb_id = d.tfb_id where d.fbk_status = 1 and a.usu_id = ?',
+			' select a.usu_nome, b.pod_nome, c.tfb_id, c.tfb_descricao, d.fbk_status from usu_usuario a join fbk_feedback d on a.usu_id  = d.usu_id join pod_podcast b on b.pod_id = d.pod_id join tfb_tipo_feedback c on c.tfb_id = d.tfb_id where a.usu_id = ?',
 			{
 				replacements: [usuid],
 				type: QueryTypes.SELECT,
@@ -123,6 +123,7 @@ class Feedback extends Model {
 
 		return results;
 	}
+
 }
 
 module.exports = Feedback;
